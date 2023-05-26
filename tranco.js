3b949be11bc5ff4f88624cfcb2a3e371e3930615
Arthur Edelstein <arthuredelstein@gmail.com>
2023-05-26 11:37:54 -0700

    tranco.js

---
+++ tranco.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const fetchText = async (url) => {
+  const response = await fetch(url);
+  return response.text();
+};
+
+const getDomains = async (count = 1000) => {
+  // First figure out what the ID of the current Tranco list is.
+  const trancoId = await fetchText('https://tranco-list.eu/top-1m-id');
+  const trancoUrl = `https://tranco-list.eu/download/${trancoId}/${count}`;
+  const trancoRs = (await fetchText(trancoUrl)).trim();
+  return {
+    source: trancoUrl,
+    domains: trancoRs.split('\r\n').map(line => line.split(',')[1])
+  };
+};
+
+module.exports = {
+  getDomains
+};
